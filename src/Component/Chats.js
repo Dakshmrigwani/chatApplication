054bdcf10dbda9fdcbe0a0b9214ccb499ac769d2
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-04-11 08:32:40 +0000

    Update 4 files

--- src/Component/Chats.js
+++ src/Component/Chats.js
@@ -1,99 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import Lightbox from "react-image-lightbox";
 import { InputGroup, Form, Button } from "react-bootstrap";
 import picture1 from "./Picture1.jpg";
 import picture3 from "./picture3.jpg";
 import picture4 from "./picture4.jpg";
 import picture5 from "./picture5.jpg";
 
+const images = [picture1, picture3, picture4, picture5];
+
 export default function Chats() {
   return (
     <>
       <div className="container-fluid py-3">
-        <div className="">
-          <InputGroup>
-            <Form.Control type="text" placeholder="Search Users" />
-            <Button variant="success">Search</Button>
-          </InputGroup>
-        </div>
+        <InputGroup>
+          <Form.Control type="text" placeholder="Search Users" />
+          <Button variant="success">Search</Button>
+        </InputGroup>
       </div>
-      <div className="">
-        <div className="d-grid">
-          <div className="row">
-            <div className="col-lg-2">
-              <img
-                src={picture1}
-                className="thumbnail rounded-circle"
-                alt=""
-                style={{ height: "3rem", width: "3rem" }}
-              />
-            </div>
-            <div className="col-lg-7">
-              <div className="row">
-                <h5>Lisa</h5>
-                <p>Hello John</p>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <p>12:00am</p>
+      <div className="d-grid">
+        <div className="row">
+          <div className="col-lg-2">
+            <img
+              src={images[0]}
+              className="thumbnail rounded-circle"
+              alt=""
+              style={{ height: "3rem", width: "3rem", cursor: "pointer" }}
+            />
+          </div>
+          <div className="col-lg-7">
+            <div className="row">
+              <h5>Lisa</h5>
+              <p>Hello John</p>
             </div>
           </div>
+          <div className="col-lg-3">
+            <p>12:00am</p>
+          </div>
+        </div>
 
-          <div className="row">
-            <div className="col-lg-2">
-              <img
-                src={picture3}
-                className="thumbnail rounded-circle"
-                alt=""
-                style={{ height: "3rem", width: "3rem" }}
-              />
-            </div>
-            <div className="col-lg-7">
-              <div className="row">
-                <h5>Kat</h5>
-                <p>Hello John</p>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <p>12:00am</p>
+        <div className="row">
+          <div className="col-lg-2">
+            <img
+              src={images[1]}
+              className="thumbnail rounded-circle"
+              alt=""
+              style={{ height: "3rem", width: "3rem", cursor: "pointer" }}
+            />
+          </div>
+          <div className="col-lg-7">
+            <div className="row">
+              <h5>Kat</h5>
+              <p>Hello John</p>
             </div>
           </div>
-          <div className="row">
-            <div className="col-lg-2">
-              <img
-                src={picture4}
-                className="thumbnail rounded-circle"
-                alt=""
-                style={{ height: "3rem", width: "3rem" }}
-              />
-            </div>
-            <div className="col-lg-7">
-              <div className="row">
-                <h5>Smith</h5>
-                <p>Hello John</p>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <p>12:00am</p>
+          <div className="col-lg-3">
+            <p>12:00am</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-2">
+            <img
+              src={images[2]}
+              className="thumbnail rounded-circle"
+              alt=""
+              style={{ height: "3rem", width: "3rem", cursor: "pointer" }}
+            />
+          </div>
+          <div className="col-lg-7">
+            <div className="row">
+              <h5>Smith</h5>
+              <p>Hello John</p>
             </div>
           </div>
-          <div className="row">
-            <div className="col-lg-2">
-              <img
-                src={picture5}
-                className="thumbnail rounded-circle"
-                alt=""
-                style={{ height: "3rem", width: "3rem" }}
-              />
+          <div className="col-lg-3">
+            <p>12:00am</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-2">
+            <img
+              src={images[3]}
+              className="thumbnail rounded-circle"
+              alt=""
+              style={{ height: "3rem", width: "3rem", cursor: "pointer" }}
+            />
+          </div>
+          <div className="col-lg-7">
+            <div className="row">
+              <h5>Alex</h5>
+              <p>Hello John</p>
             </div>
-            <div className="col-lg-7">
-              <div className="row">
-                <h5>Alex</h5>
-                <p>Hello John</p>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <p>12:00am</p>
-            </div>
+          </div>
+          <div className="col-lg-3">
+            <p>12:00am</p>
           </div>
         </div>
       </div>
