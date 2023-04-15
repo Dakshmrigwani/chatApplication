13a7d90edbf9226a75759aa3ae56cf2e268c7bd5
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-04-15 14:26:42 +0000

    Update 2 files

--- src/Component/Message/Store.js
+++ src/Component/Message/Store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import messageReducer from "./messageSlice";
 import buttonReducer from "./buttonSlice";
-import profileReducer from "./profileSlice";
 
 export default configureStore({
   reducer: {
     message: messageReducer,
     button: buttonReducer,
-    profile: profileReducer,
   },
 });
